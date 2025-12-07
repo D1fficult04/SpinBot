7391d186376c82386e9f0173c9589dd5f4358f3a
D1fficult04 <yandere2022@gmail.com>
2025-12-07 03:43:58 +0100

    Add files via upload

---
+++ Spinbot OP.lua
@@ -0,0 +1,6 @@
+power = 500 -- change this to make it more or less powerful
+
+local bambam = Instance.new("BodyThrust")
+bambam.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
+bambam.Force = Vector3.new(power,0,power)
+bambam.Location = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
